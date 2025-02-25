2f4bec4ce6bc351f3aea6add4e44001681133208
davigrigolin <davigrigolin@gmail.com>
2025-02-25 00:43:01 -0300

    Alterações no main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const qrcode = require('qrcode-terminal'); 
+const { Client, LocalAuth } = require('whatsapp-web.js');
+//const userSessions = {};
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED');
+    qrcode.generate(qr, {small: true});
+});
+
+
+client.on('message_create', message => {
+    if (['oi', 'Oi', 'OI', 'olá', 'ola', 'Ola', 'Dia', 'dia', 'Noite', 'noite', 'tarde', 'Tarde', 'Bom dia', 'Boa', 'bom dia', 'boa'].includes(message.body)) {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'Olá, você está em contato com a Congrese Sistemas de Segurança!\nDigite o número do departamento para contato.');
+        client.sendMessage(message.from, '1️⃣ - camera e alarme.\n2️⃣ - G2 cabos.\n3️⃣ - Guardian Life.\nÉ possível digitar "menu" para voltar à essa mensagem.');
+	}
+});
+
+// Start your client
+client.initialize();
